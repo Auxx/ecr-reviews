725807e78b870367079642bc21c5528a51240f62
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2015-01-03 17:41:24 +0200

    Working on fetching posts from Reddit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
 
-#  def self.
+  def self.fetch_posts
+    response = Faraday.get('http://www.reddit.com/r/ecr_eu/search.json', {:q => 'flair:review', :restrict_sr => 'on', :sort => 'new', :t => 'all'})
+    response.body
+  end
 
 end
